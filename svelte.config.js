70be9612097a13ad8585592bfa7e5cecd3aedd8a
Lee Knowlton <Lee Knowlton>
2022-01-20 10:13:12 +1300

    init

---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+// svelte.config.js
+
+const sveltePreprocess = require("svelte-preprocess");
+
+module.exports = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess(),
+};
